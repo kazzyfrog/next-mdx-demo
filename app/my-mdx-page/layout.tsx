996c5df6c8444eb4dae355902a77d14db54d8265
kazzyfrog <kazkihondo@icloud.com>
2024-02-14 21:11:45 +0900

    chore:gear:setting up rehype-prism by syntax highlight

--- app/my-mdx-page/layout.tsx
+++ app/my-mdx-page/layout.tsx
@@ -1,3 +1,11 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <article className="prose prose-xl">{children}</article>;
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/themes/prism-okaidia.min.css"
+      />
+      <article className="prose prose-base">{children}</article>
+    </>
+  );
 }
